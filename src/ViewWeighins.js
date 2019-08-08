5fbfecfe879ecd537b8eec9d9bc2e2ab283a5c5b
Chris Westbrook <westbchris@gmail.com>
2019-08-08 19:57:37 -0400

    GOt view weigh ins component converted to material ui

--- src/ViewWeighins.js
+++ src/ViewWeighins.js
@@ -1,51 +1,74 @@
-import React, { useContext } from 'react';
-import moment from 'moment';
-import { useCollection } from 'react-firebase-hooks/firestore';
-import { UserContext } from './firebase/FirebaseUser';
-import { FirebaseContext } from './firebase/firebase';
-import useInitialfocus from './hooks/useInitialFocus';
-const Viewweighins = () => {
-	const main = React.createRef(null);
-	const user = useContext(UserContext);
-	const firebase = useContext(FirebaseContext);
+import React, { useContext } from "react";
+import moment from "moment";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { UserContext } from "./firebase/FirebaseUser";
+import { FirebaseContext } from "./firebase/firebase";
+import useInitialfocus from "./hooks/useInitialFocus";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
-	const [weighIns, loading, error] = useCollection(
-		firebase.db
-			.collection('weighins')
-			.where('uid', '==', user.uid ? user.uid : '')
-			.orderBy('date')
-	);
-	useInitialfocus(main, 'View Weigh-Ins');
-	return (
-		<div>
-			<h1 ref={main} tabIndex="-1">
-				View Weighins
-			</h1>
-			{loading && <div>Loading...</div>}
-			{error && <div>Error:{error}</div>}
-			{weighIns && (
-				<table>
-					<tbody>
-						<tr>
-							<td>Date</td>
-							<td>Weight</td>
-							<td>Change Since Last Weigh-in</td>
-						</tr>
-						{weighIns.docs.map(doc => {
-							const data = doc.data();
-							const { date, weight, difference } = data;
-							return (
-								<tr key={doc.id}>
-									<td>{moment(date.toDate()).format('MM/DD/YYYY')}</td>
-									<td>{weight}</td>
-									<td>{difference}</td>
-								</tr>
-							);
-						})}
-					</tbody>
-				</table>
-			)}
-		</div>
-	);
+const Viewweighins = () => {
+  const useStyles = makeStyles(theme => ({
+    root: {
+      width: "100%",
+      marginTop: theme.spacing(3),
+      overflowX: "auto"
+    },
+    table: {
+      minWidth: 650
+    }
+  }));
+  const main = React.createRef(null);
+  const user = useContext(UserContext);
+  const firebase = useContext(FirebaseContext);
+
+  const [weighIns, loading, error] = useCollection(
+    firebase.db
+      .collection("weighins")
+      .where("uid", "==", user.uid ? user.uid : "")
+      .orderBy("date")
+  );
+  useInitialfocus(main, "View Weigh-Ins");
+  const classes = useStyles();
+  return (
+    <Paper className={classes.root}>
+      <h1 ref={main} tabIndex="-1">
+        View Weighins
+      </h1>
+      {loading && <div>Loading...</div>}
+      {error && <div>Error:{error}</div>}
+      {weighIns && (
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Date</TableCell>
+              <TableCell>Weight</TableCell>
+              <TableCell>Change Since Last Weigh-in</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {weighIns.docs.map(doc => {
+              const data = doc.data();
+              const { date, weight, difference } = data;
+              return (
+                <TableRow key={doc.id}>
+                  <TableCell>
+                    {moment(date.toDate()).format("MM/DD/YYYY")}
+                  </TableCell>
+                  <TableCell>{weight}</TableCell>
+                  <TableCell>{difference}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      )}
+    </Paper>
+  );
 };
 export default Viewweighins;
