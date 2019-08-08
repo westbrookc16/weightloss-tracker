e5d4b22ee73449cd631964f6f2e43ec27e2119bf
Chris Westbrook <westbchris@gmail.com>
2019-08-08 17:43:04 -0400

    Made some more changes.

--- src/About.js
+++ src/About.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import useInitialfocus from './hooks/useInitialFocus';
+import React, { useRef } from "react";
+import useInitialfocus from "./hooks/useInitialFocus";
+
 const About = () => {
-	const main = React.createRef(null);
-	useInitialfocus(main, 'About');
-	return (
-		<div>
-			<h1 tabIndex="-1" ref={main}>
-				About
-			</h1>
-			this app is just a quick demo I built to showcase my technical skills. It allows you to track weigh ins and
-			track your progress from your last weight. Have fun.
-		</div>
-	);
+  const main = useRef(null);
+  useInitialfocus(main, "About");
+  return (
+    <div>
+      <h1 tabIndex="-1" ref={main}>
+        About
+      </h1>
+      this app is just a quick demo I built to showcase my technical skills. It
+      allows you to track weigh ins and track your progress from your last
+      weight. Have fun.
+    </div>
+  );
 };
 export default About;
