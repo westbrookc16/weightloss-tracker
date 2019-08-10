3a72a17bf95d7b82f7fdcf19cac773c0c4365b7c
Chris Westbrook <westbchris@gmail.com>
2019-08-10 09:26:56 -0400

    Implemented React.Lazy and suspense.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import React from 'react';
-import ViewWeighIns from './ViewWeighins';
-import { firebase, FirebaseContext } from './firebase/firebase.js';
-import { FirebaseUser } from './firebase/FirebaseUser';
-import Home from './Home';
-import Weighin from './Weighin';
-import About from './About';
-import Nav from './Nav';
-import SignIn from './SignIn';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { lazy, Suspense } from "react";
 
-import './App.css';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
+import { firebase, FirebaseContext } from "./firebase/firebase.js";
+import { FirebaseUser } from "./firebase/FirebaseUser";
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "./App.css";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+
+const Nav = lazy(() => import("./Nav"));
+const Home = lazy(() => import("./Home"));
+const Weighin = lazy(() => import("./Weighin"));
+const About = lazy(() => import("./About"));
+const SignIn = lazy(() => import("./SignIn"));
+const ViewWeighIns = lazy(() => import("./ViewWeighins"));
 function App() {
-	return (
-		<Container maxWidth="sm">
-			<FirebaseContext.Provider value={new firebase()}>
-				<FirebaseUser>
-					<Router>
-						<Nav />
-						<Box my={4}>
-							<Route path="/" exact component={Home} />
-							<Route path="/signin" component={SignIn} />
-							<Route path="/weighin" component={Weighin} />
-							<Route path="/about" component={About} />
-							<Route path="/viewWeighins" component={ViewWeighIns} />
-						</Box>
-					</Router>
-				</FirebaseUser>
-			</FirebaseContext.Provider>
-		</Container>
-	);
+  return (
+    <Container maxWidth="sm">
+      <FirebaseContext.Provider value={new firebase()}>
+        <FirebaseUser>
+          <Router>
+            <Suspense fallback={() => <>Loading...</>}>
+              <Nav />
+              <Box my={4}>
+                <Route path="/" exact component={Home} />
+                <Route path="/signin" component={SignIn} />
+                <Route path="/weighin" component={Weighin} />
+                <Route path="/about" component={About} />
+                <Route path="/viewWeighins" component={ViewWeighIns} />
+              </Box>
+            </Suspense>
+          </Router>
+        </FirebaseUser>
+      </FirebaseContext.Provider>
+    </Container>
+  );
 }
 
 export default App;
